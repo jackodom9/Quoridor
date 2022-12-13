38a528117d9b1ce48791b110ca5912359768d26e
jackodom9 <jeo0011@auburn.edu>
2022-12-12 19:35:23 -0600

    Added base file, class, and cmdline output.

--- Quoridor.py
+++ Quoridor.py
@@ -1,1 +1,12 @@
+import pygame as pg
 
+class Game:
+
+    def __init__(self):
+        self.board = [[0 for i in range(9)] for j in range(9)]
+        self.board[0][4] = 2
+        self.board[8][4] = 1
+
+game = Game()
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in game.board]))
